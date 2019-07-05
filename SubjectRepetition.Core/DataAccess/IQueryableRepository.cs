b7e244a2aeb0ffd4b5c27e82ff3a598bbe9266c0
yunver <yunus.unver1995@gmail.com>
2019-07-06 00:27:44 +0300

    Added Transaction Aspects

--- SubjectRepetition.Core/DataAccess/IQueryableRepository.cs
+++ SubjectRepetition.Core/DataAccess/IQueryableRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SubjectRepetition.Core.Entities;
 
 namespace SubjectRepetition.Core.DataAccess
 {
-    public interface IQueryableRepository<T> where T : class, T, new()
+    public interface IQueryableRepository<T> where T : class, IEntity, new()
     {
         IQueryable<T> GetEntities();
     }
