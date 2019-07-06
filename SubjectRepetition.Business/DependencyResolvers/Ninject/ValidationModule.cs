fc0b5d5286ee03d483317f6355bca966471d835e
yunver <yunus.unver1995@gmail.com>
2019-07-07 00:20:43 +0300

    Added Validation Module

---
+++ SubjectRepetition.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Ninject.Modules;
+using SubjectRepetition.Business.ValidationRules.FluentValidation;
+using SubjectRepetition.Entities.Concrete;
+
+namespace SubjectRepetition.Business.DependencyResolvers.Ninject
+{
+    public class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Category>>().To<CategoryValidator>();
+            Bind<IValidator<Product>>().To<ProductValidator>();
+        }
+    }
+}
