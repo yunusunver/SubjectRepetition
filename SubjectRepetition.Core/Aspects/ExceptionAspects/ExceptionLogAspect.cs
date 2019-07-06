cb886dea0dedfbcd1b667ab420658ee74bd44c86
yunver <yunus.unver1995@gmail.com>
2019-07-06 15:28:28 +0300

    Added Exception Log Aspect

---
+++ SubjectRepetition.Core/Aspects/ExceptionAspects/ExceptionLogAspect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+using SubjectRepetition.Core.CrossCuttingConcerns.Logging;
+using SubjectRepetition.Core.CrossCuttingConcerns.Logging.Log4Net;
+
+namespace SubjectRepetition.Core.Aspects.ExceptionAspects
+{
+    public class ExceptionLogAspect:OnExceptionAspect
+    {
+        private Type _loggerType;
+        private LoggerService _loggerService;
+
+        public ExceptionLogAspect(Type loggerType)
+        {
+            _loggerType = loggerType;
+        }
+
+        public override void RuntimeInitialize(MethodBase method)
+        {
+            if (_loggerType.BaseType != typeof(LoggerService))
+            {
+                throw new Exception("Wrong");
+            }
+            _loggerService = (LoggerService)Activator.CreateInstance(_loggerType);
+            base.RuntimeInitialize(method);
+        }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            if (_loggerService != null)
+            {
+                _loggerService.Error(args.Exception);
+            }
+        }
+    }
+}
